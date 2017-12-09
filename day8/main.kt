cc9846c36bcf93ebd60d56c9bdd3cce608914203
Daniel <daniellangbehn@gmx.de>
2017-12-09 08:14:31 +0100

    Update main.kt

--- day8/main.kt
+++ day8/main.kt
@@ -1,32 +1,47 @@
 import java.io.File
 
-//this is part2
-
-val f = File("""C:\Users\Daniel\Downloads\Neuer Ordner\adventofcode\input.txt""").readLines().filter { !it.isEmpty() }
 
 fun main(args: Array<String>) {
-    val p = createProcess("eugwuhl")
-    var unbalanceChild = p.unbalancedChild
-    while (unbalanceChild != null){
-        if (unbalanceChild.first.unbalancedChild == null) println("${unbalanceChild.first.name} (${unbalanceChild.first.weight}) ${unbalanceChild.second} = ${unbalanceChild.first.weight + unbalanceChild.second}")
-        unbalanceChild = unbalanceChild.first.unbalancedChild
+    val registers = mutableMapOf<String, Int>()
+    var max = 0
+    fun readRegister(register:String):Int = if (register in registers.keys) registers[register]!! else 0
+    fun setRegister(register:String, value:Int) {
+        if (value>max) max = value
+        if (register in registers.keys) registers[register] = value
+        else registers.put(register, value)
     }
+    fun row(data:String):Row{
+        val r = data.split(" if ")[0].split(" ")[0]
+        val i = data.split(" if ")[0].split(" ")[1]
+        val v = data.split(" if ")[0].split(" ")[2].toInt()
+
+        val lV = data.split(" if ")[1].split(" ")[0]
+        val rV = data.split(" if ")[1].split(" ")[2]
+        val c = data.split(" if ")[1].split(" ")[1]
+        return Row(r, i, v, Condition(lV, rV, c))
+    }
+    fun value2int(value:String):Int = if (value[0] in 'a'..'z') readRegister(value) else value.toInt()
+    fun compute(row:Row){
+        var b:Boolean = when(row.condition.condition) {
+            ">" -> value2int(row.condition.leftValue) > value2int(row.condition.rightValue)
+            ">=" -> value2int(row.condition.leftValue) >= value2int(row.condition.rightValue)
+            "<" -> value2int(row.condition.leftValue) < value2int(row.condition.rightValue)
+            "<=" -> value2int(row.condition.leftValue) <= value2int(row.condition.rightValue)
+            "==" -> value2int(row.condition.leftValue) == value2int(row.condition.rightValue)
+            "!=" -> value2int(row.condition.leftValue) != value2int(row.condition.rightValue)
+            else -> false
+        }
+        if (b){
+            setRegister(row.register, readRegister(row.register) + if(row.instruction == "inc") value2int(row.value.toString()) else -value2int(row.value.toString()))
+        }
+    }
+    val data = File("""C:\Users\Daniel\Downloads\Neuer Ordner\adventofcode\input.txt""").readLines().filter { !it.isEmpty() }
+    for (row in data){
+        compute(row(row))
+    }
+    println("Part1: ${registers.maxBy { it.value }}")
+    println("Part2: $max")
 }
 
-data class Process(val name:String, var weight:Int, val childrens:MutableList<Process>, var sum:Int, var unbalancedChild:Pair<Process, Int>? = null)
-fun Process.getWeight(){ for (row in f) if (row.split(" ")[0] == this.name) this.weight = row.split("(")[1].split(")")[0].toInt() }
-fun Process.getChildrens(){ for (row in f) if (row.split(" ")[0] == this.name && row.split("->").count() == 2) for (child in row.split("->")[1].split(",")) this.childrens.add(createProcess(child.trim())) }
-fun Process.getSum(){
-    this.sum = this.weight
-    for (c in childrens) this.sum += c.sum
-}
-fun Process.unbalancedChild(){ for (c in this.childrens) if (this.childrens.filter { it.sum == c.sum }.count() == 1) this.unbalancedChild = Pair(c, this.childrens.filter({ it.sum != c.sum })[0].sum - c.sum) }
-
-fun createProcess(name:String):Process{
-    val res = Process(name, 0, mutableListOf(), 0)
-    res.getWeight()
-    res.getChildrens()
-    res.getSum()
-    res.unbalancedChild()
-    return res
-}
+data class Row(val register:String, val instruction:String, val value:Int, val condition:Condition)
+data class Condition(val leftValue:String, val rightValue:String, val condition:String)
